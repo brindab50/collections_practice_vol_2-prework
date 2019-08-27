78d231c1f34dcd9494898cfde3009d54a3006525
brinda behal <brindab50@gmail.com>
2019-08-27 19:11:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -65,16 +65,17 @@ end
     
   end 
    
-def organize_campuses(array)
-  new_array = []
-  array.each do |element|
-   new_array << element[:location] == element[:campuses]
+def organize_campuses(hash)
+  new_hash = []
+  hash.each do |element, campuses|
+  
+  # binding.pry
+   new_hash << campuses[:location]
+   end 
     
-  end 
-    
-    return new_array
+    return new_hash
 end 
-    
+  end 
     
     
     
